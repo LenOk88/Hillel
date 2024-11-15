a6d32aadc1054ba2cc21bf81ccf2380e23d597c2
Hillel <shevchesp70@gmail.com>
2024-11-15 09:35:22 +0100

    new file

---
+++ Homework3/Homework3.2/index.js
@@ -0,0 +1,6 @@
+const message1 = prompt("Message1", "Write a message here");
+const message2 = prompt("Message2", "Write a message here");
+const message3 = prompt("Message3", "Write a message here");
+alert
+(`Message1: ${message1}\nMessage2: ${message2}\nMessage3: ${message3}`,
+);
