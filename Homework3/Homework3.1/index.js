ce6a616a48debea7270775fae92f16ed180ced94
Hillel <shevchesp70@gmail.com>
2024-11-17 19:36:04 +0100

    new homwork

--- Homework3/Homework3.1/index.js
+++ Homework3/Homework3.1/index.js
@@ -1,13 +1,8 @@
 console.log(typeof undefined);
 console.log(typeof 0);
-console.log(typeof 10e2);
+console.log(typeof 10e6);
 console.log(typeof true);
-console.log(typeof false);
 console.log(typeof Symbol('id'));
 console.log(typeof 'Lena');
 console.log(typeof []);
-console.log(typeof {});
-console.log(typeof function(){});
 console.log(typeof null);
-console.log(typeof Math);
-console.log(typeof String);
--- Homework3/Homework3.3/index.js
+++ Homework3/Homework3.3/index.js
@@ -1,2 +1,6 @@
-let str = `1 ${''}0 ${''}3 ${''}6 ${''}9 ${''}`;
-console.log(str);
+// let str = `1 ${''}0 ${''}3 ${''}6 ${''}9 ${''}`;
+// console.log(str);
+
+const digit = String(10369);
+const number5 = digit[0] + " " + digit[1] + " " + digit[2] + " " + digit[3] + " " + digit[4];
+console.log(number5);
