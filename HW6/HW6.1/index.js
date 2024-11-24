1f084d11d175053bcddcb72da5416eb9ead4f929
Hillel <shevchesp70@gmail.com>
2024-11-24 17:43:42 +0100

    hw6.1

--- HW6/HW6.1/index.js
+++ HW6/HW6.1/index.js
@@ -1,11 +1,30 @@
 
-const message = prompt('Enter your string');
-const chars = prompt('Enter some chars for removing').split('');
+// const message = prompt('Enter your string');
+// const chars = prompt('Enter some chars for removing').split('');
 
-function foo(string, arr) {
-    return string.split('').filter(char => !arr.includes(char)).join('');
+// function foo(string, arr) {
+//     return string.split('').filter(char => !arr.includes(char)).join('');
+// }
+
+// const result = foo(message, chars);
+
+// alert(result);
+
+
+
+const message = prompt('Enter your string'); 
+if (!message) {
+    alert('You did not enter a string.');
+} else {
+    const chars = prompt('Enter some chars for removing').split('');
+    if (!chars.length) {
+        alert('You did not enter any characters to remove.');
+    } else {
+        function foo(string, arr) {
+            return string.split('').filter(char => !arr.includes(char)).join('');
+        }
+
+        const result = foo(message, chars);
+        alert(result);
+    }
 }
-
-const result = foo(message, chars);
-
-alert(result);
