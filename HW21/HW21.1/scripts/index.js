981c567bfcee8cc578107d9921cd576488ff945e
Hillel <shevchesp70@gmail.com>
2025-01-26 20:12:27 +0100

    hw21

---
+++ HW21/HW21.1/scripts/index.js
@@ -0,0 +1,26 @@
+$(document).ready(() => {
+  const taskList = $('#taskList');
+  const taskInput = $('#taskInput');
+  const addTaskButton = $('#addTaskButton');
+
+  addTaskButton.on('click', () => {
+    const taskText = taskInput.val().trim();
+    if (taskText) {
+      const newTask = $(`<li>${taskText} <button class='deleteBtn btn btn-danger'>Видалити</button></li>`);
+      taskList.append(newTask);
+      taskInput.val('').focus();
+    }
+  });
+
+  taskList.on('click', '.deleteBtn', function () {
+    $(this).closest('li').remove();
+  });
+
+  taskList.on('click', 'li', function (e) {
+    if (!$(e.target).hasClass('deleteBtn')) {
+      const taskText = $(this).text().replace('Видалити', '').trim();
+      $('#taskText').text(taskText);
+      $('#taskModal').modal('show');
+    }
+  });
+});
